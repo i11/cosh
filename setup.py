3e8f49d971bbcbd4807f811238ea422c569f479c
Ilja Bobkevic <ilja@spotify.com>
2018-07-27 11:40:26 +0200

    Add deploy section. Version from tag

--- setup.py
+++ setup.py
@@ -8,22 +8,36 @@ with open('README.md') as f:
 with open('LICENSE') as f:
   license = f.read()
 
+version_format = '{tag}'
+url='https://github.com/i11/cosh',
+
 setup(
   name='cosh',
-  version='0.1.0',
+  version_format=version_format,
   description='Container Shell',
   long_description=readme,
   author='Ilja Bobkevic',
   author_email='ilja@bobkevic.com',
-  url='https://github.com/i11/cosh',
+  url=url,
   license=license,
   packages=find_packages(exclude=('tests', 'docs')),
   scripts=[
     'bin/cosh'
   ],
+  setup_requires=[
+    'setuptools-git-version==1.0.3'
+  ],
   install_requires=[
     'docker==3.4.1',
     'argparse==1.4.0',
     'requests==2.19.1'
+  ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Natural Language :: English',
+    'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3.6',
   ]
 )
